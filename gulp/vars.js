02793d3a9787cf89edb287073117deeefd588dfd
Aleksander Grigorovich <aleksander.grigorovich@altenar.com>
2025-01-22 23:09:02 +0300

    add new bg video

--- gulp/vars.js
+++ gulp/vars.js
@@ -1,31 +1,27 @@
 const dirs = {
-    public: 'public/**/*.*',
-    src: {
-        pages: [
-            'src/pages/*.html',
-            '!src/pages/_*.html',
-        ],
-        styles: [
-            'src/styles/*.scss',
-            '!src/styles/_*.scss',
-        ],
-        scripts: "src/scripts/*.js",
-        img: "src/img/**/*.*",
-        fonts: "src/fonts/**/*.*",
-    },
-    dist: {
-        root: "dist",
-        styles: "dist/styles",
-        scripts: "dist/scripts",
-        img: "dist/img/",
-        fonts: "dist/fonts",
-    },
-    watch: {
-        pages: "src/pages/**/*.html",
-        styles: "src/styles/**/*.scss",
-        scripts: "src/scripts/**/*.js",
-        img: "src/img/**/*.*",
-    }
+  public: 'public/**/*.*',
+  src: {
+    pages: ['src/pages/*.html', '!src/pages/_*.html'],
+    styles: ['src/styles/*.scss', '!src/styles/_*.scss'],
+    scripts: 'src/scripts/*.js',
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*',
+    video: 'src/video/**/*.*',
+  },
+  dist: {
+    root: 'dist',
+    styles: 'dist/styles',
+    scripts: 'dist/scripts',
+    img: 'dist/img/',
+    fonts: 'dist/fonts',
+    video: 'dist/video',
+  },
+  watch: {
+    pages: 'src/pages/**/*.html',
+    styles: 'src/styles/**/*.scss',
+    scripts: 'src/scripts/**/*.js',
+    img: 'src/img/**/*.*',
+  },
 }
 
 export { dirs }
--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,111 @@
-import gulp from 'gulp';
-import { nunjucksCompile } from 'gulp-nunjucks';
-import imagemin from 'gulp-imagemin';
-import esbuild from 'gulp-esbuild';
-import autoprefixer from 'gulp-autoprefixer';
+import gulp from 'gulp'
+import { nunjucksCompile } from 'gulp-nunjucks'
+import imagemin from 'gulp-imagemin'
+import esbuild from 'gulp-esbuild'
+import autoprefixer from 'gulp-autoprefixer'
 import { sassPlugin } from 'esbuild-sass-plugin'
-import postcss from 'esbuild-postcss';
-import pxtorem from 'postcss-pxtorem';
-import sortMQ from 'postcss-sort-media-queries';
-import combineMQ from 'postcss-combine-media-query';
-import browserSync from 'browser-sync';
-browserSync.create();
+import postcss from 'esbuild-postcss'
+import pxtorem from 'postcss-pxtorem'
+import sortMQ from 'postcss-sort-media-queries'
+import combineMQ from 'postcss-combine-media-query'
+import browserSync from 'browser-sync'
+browserSync.create()
 
-import rimraf from 'gulp-rimraf';
-import newer from 'gulp-newer';
-import { dirs } from './gulp/vars.js';
-
-
+import rimraf from 'gulp-rimraf'
+import newer from 'gulp-newer'
+import { dirs } from './gulp/vars.js'
 
 /* ---------- HTML ---------- */
 
 function buildHtml(cb) {
-    return gulp.src(dirs.src.pages)
-        .pipe(nunjucksCompile())
-        .pipe(gulp.dest(dirs.dist.root))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(dirs.src.pages)
+    .pipe(nunjucksCompile())
+    .pipe(gulp.dest(dirs.dist.root))
+    .pipe(browserSync.stream())
 }
 async function watchHtml(cb) {
-    return gulp.watch(dirs.watch.pages, buildHtml);
+  return gulp.watch(dirs.watch.pages, buildHtml)
 }
-
-
 
 /* ---------- STYLES ---------- */
 
 function buildStyles() {
-    const options = {
+  const options = {
+    plugins: [
+      sassPlugin(),
+      postcss({
         plugins: [
-            sassPlugin(),
-            postcss({
-                plugins: [
-                    combineMQ(),
-                    sortMQ({ sort: 'desktop-first' }),
-                    pxtorem({
-                        replace: false,
-                        rootValue: 16,
-                        propList: ['*'],
-                    }),
-                ]
-            }),
+          combineMQ(),
+          sortMQ({ sort: 'desktop-first' }),
+          pxtorem({
+            replace: false,
+            rootValue: 16,
+            propList: ['*'],
+          }),
         ],
-    };
-    return gulp.src(dirs.src.styles)
-        .pipe(esbuild(options))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest(dirs.dist.styles))
-        .pipe(browserSync.stream());
+      }),
+    ],
+  }
+  return gulp
+    .src(dirs.src.styles)
+    .pipe(esbuild(options))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(dirs.dist.styles))
+    .pipe(browserSync.stream())
 }
 
 async function watchStyles(cb) {
-    return gulp.watch(dirs.watch.styles, buildStyles);
+  return gulp.watch(dirs.watch.styles, buildStyles)
 }
-
 
 /* ---------- SCRIPTS ---------- */
 
 function buildScripts() {
-    const options = {
-        bundle: true,
-        minify: true,
-    };
-    return gulp.src(dirs.src.scripts)
-        .pipe(esbuild(options))
-        .pipe(gulp.dest(dirs.dist.scripts))
-        .pipe(browserSync.stream());
+  const options = {
+    bundle: true,
+    minify: true,
+  }
+  return gulp
+    .src(dirs.src.scripts)
+    .pipe(esbuild(options))
+    .pipe(gulp.dest(dirs.dist.scripts))
+    .pipe(browserSync.stream())
 }
 
 async function watchScripts(cb) {
-    return gulp.watch(dirs.watch.scripts, buildScripts);
+  return gulp.watch(dirs.watch.scripts, buildScripts)
 }
-
 
 /* ---------- IMAGES ---------- */
 
 function buildImages() {
-    return gulp.src(dirs.src.img)
-        .pipe(newer(dirs.dist.img))
-        .pipe(imagemin())
-        .pipe(gulp.dest(dirs.dist.img));
+  return gulp
+    .src(dirs.src.img)
+    .pipe(newer(dirs.dist.img))
+    .pipe(imagemin())
+    .pipe(gulp.dest(dirs.dist.img))
 }
 async function watchImages(cb) {
-    return gulp.watch(dirs.watch.img, buildImages);
+  return gulp.watch(dirs.watch.img, buildImages)
 }
-
 
 /* ---------- ASSETS ---------- */
 
 function copyFonts() {
-    return gulp.src(dirs.src.fonts)
-        .pipe(gulp.dest(dirs.dist.fonts));
+  return gulp.src(dirs.src.fonts).pipe(gulp.dest(dirs.dist.fonts))
+}
+
+function copyImages() {
+  return gulp.src(dirs.src.img).pipe(gulp.dest(dirs.dist.img))
+}
+
+function copyVideo() {
+  return gulp.src(dirs.src.video).pipe(gulp.dest(dirs.dist.video))
 }
 
 function copyPublicAssets() {
-    return gulp.src(dirs.public)
-        .pipe(gulp.dest(dirs.dist.root));
+  return gulp.src(dirs.public).pipe(gulp.dest(dirs.dist.root))
 }
 
 // function copyAssets() {
@@ -110,42 +113,50 @@ function copyPublicAssets() {
 //         .pipe(gulp.dest(dirs.dist.root));
 // }
 
-let copyAssets = gulp.parallel(copyFonts, copyPublicAssets);
+const copyAssets = gulp.parallel(
+  copyFonts,
+  copyPublicAssets,
+  copyImages,
+  copyVideo
+)
 
 async function watchAssets(cb) {
-    return gulp.watch(dirs.public, copyAssets);
+  return gulp.watch(dirs.public, copyAssets)
 }
-
 
 /* ---------- HELPERS ---------- */
 function clean(cb) {
-    return gulp.src(dirs.dist.root)
-        .pipe(rimraf());
+  return gulp.src(dirs.dist.root).pipe(rimraf())
 }
 
 async function serve(cb) {
-    return browserSync.init({
-        server: {
-            baseDir: dirs.dist,
-        }
-    });
+  return browserSync.init({
+    server: {
+      baseDir: dirs.dist,
+    },
+  })
 }
-
 
 /* ---------- TASKS ---------- */
 
 const dev = gulp.series(
-    gulp.parallel(copyAssets, buildImages, buildStyles, buildScripts),
-    buildHtml,
-    gulp.parallel(watchAssets, watchImages, watchHtml, watchStyles, watchScripts, serve),
-);
+  gulp.parallel(copyAssets, buildImages, buildStyles, buildScripts),
+  buildHtml,
+  gulp.parallel(
+    watchAssets,
+    watchImages,
+    watchHtml,
+    watchStyles,
+    watchScripts,
+    serve
+  )
+)
 
 const build = gulp.series(
-    clean,
-    copyAssets,
-    gulp.parallel(buildStyles, buildScripts, buildHtml),
-);
-
+  clean,
+  copyAssets,
+  gulp.parallel(buildStyles, buildScripts, buildHtml)
+)
 
 export { dev, clean }
-export default build;
+export default build
